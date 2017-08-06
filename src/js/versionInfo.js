52611f18dac9ff12fed2063942f4e1b15df13aa2
ccai1 <cathy@Cathys-MacBook-Pro.local>
2017-08-06 18:33:33 -0400

    navbar css

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.756' };
+let VersionInfo = { VERSION : '0.0.757' };
 export default VersionInfo
