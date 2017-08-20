74c7e61655076ba01f62e6a56ee34f533d916a9d
ccai1 <ccai1@stuy.edu>
2017-08-19 21:07:45 -0400

    added gridding and more css

--- src/js/modules/forms/components/Confirmation.js
+++ src/js/modules/forms/components/Confirmation.js
@@ -4,21 +4,34 @@ import injectSheet            from 'react-jss';
 import ArticleEditor from "./ArticleEditor";
 import ContributorsList       from './ContributorsList';
 import ContributorsInput      from "./EditorInputs/ContributorsInput";
+import { Col, Grid, Row } from 'react-bootstrap/lib/';
 
 const styles = {
+  wrapper: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    gridGap: '5px',
+    gridAutoRows: 'minmax(50px, auto)',
+  },
   title: {
     textAlign: 'center',
     fontFamily: "Roboto, sans-serif",
     paddingTop: '30px',
+    gridColumn: '2/5',
+    gridRow: '1'
   },
   text: {
     paddingTop: '20px',
     textAlign: 'center',
+    gridColumn: '2/5',
+    gridRow: '2',
   },
   minionImage: {
     display: 'block',
     margin: '0 auto',
     paddingTop: '30px',
+    gridColumn: '2/5',
+    gridRow: '3',
   }
 }
 
@@ -26,11 +39,11 @@ class Confirmation extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
+      <div className={classes.wrapper}>
       <h1 className={classes.title}> Confirmation Page </h1>
       <h3 className={classes.text}>Your article has been submitted.</h3>
-        <img src="http://diysolarpanelsv.com/images/bob-the-minion-clipart-transparent-background-38.gif" alt="Minion"
-        height="80%" width="80%" className={classes.minionImage}></img>
+        <img src="https://s-media-cache-ak0.pinimg.com/originals/36/b1/6c/36b16cc68bbbca07f1c37183112de693.gif" alt="Minion"
+        height='400px' width='400px' className={classes.minionImage}></img>
       </div>
     );
   }
--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.791' };
+let VersionInfo = { VERSION : '0.0.823' };
 export default VersionInfo
